dcbf04836492f8a33a23a088784cf21d506d3229
Subhajitorrin <orrin2op@gmail.com>
2024-04-12 22:44:25 +0530

    edit btn

--- src/Components/UserInfo.jsx
+++ src/Components/UserInfo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./UserInfo.css";
 import { IoIosVideocam } from "react-icons/io";
 import { IoIosCall } from "react-icons/io";
@@ -7,12 +7,27 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 function UserInfo({ user }) {
+  const [toggleEdit, setToggleEdit] = useState(false);
+  const updateRef = useRef(null)
+  function handelEditBtn(){
+    if(updateRef.current.classList.contains("updateActive")){
+      updateRef.current.classList.remove("updateActive")
+    }else{
+      updateRef.current.classList.add("updateActive")
+    }
+  }
   return (
     <div className="userinfoContainer">
       <div className="user">
         <div className="userImgContainer">
           <img
-            src={user ? user.avatarUrl : <Skeleton width={40} height={40} circle={true}/>}
+            src={
+              user ? (
+                user.avatarUrl
+              ) : (
+                <Skeleton width={40} height={40} circle={true} />
+              )
+            }
             alt=""
           />
         </div>
@@ -21,7 +36,34 @@ function UserInfo({ user }) {
       <div className="icons">
         <IoIosVideocam className="reactIcons" />
         <IoIosCall className="reactIcons" />
-        <FaRegEdit className="reactIcons" />
+        <div className="editContainer">
+          <FaRegEdit
+            className="reactIcons"
+            onClick={handelEditBtn}
+          />
+          <div className="editProfileContainer updateActive" ref={updateRef}>
+            <div className="updateImgContainer">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVCe_lWmhlCdO7BloszzyO1iPjApWMgitzkuB5ezk3Ig&s"
+                alt=""
+              />
+            </div>
+            <label htmlFor="editavatar">Upload your avatar</label>
+            <input
+              type="file"
+              name="editavatar"
+              id="editavatar"
+              style={{ display: "none" }}
+            />
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter your new username"
+            />
+            <button>Update profile</button>
+          </div>
+        </div>
       </div>
     </div>
   );
