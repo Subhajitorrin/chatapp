69610bac46fc25c83b2b320b9125ff607729f440
Subhajitorrin <orrin2op@gmail.com>
2024-04-12 15:24:22 +0530

    chat bottom and up done

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,12 +1,80 @@
-import React from 'react'
-import "./Chat.css"
+import React, { useState } from "react";
+import "./Chat.css";
+import { IoIosCall } from "react-icons/io";
+import { IoIosVideocam } from "react-icons/io";
+import { ImInfo } from "react-icons/im";
+import { CiImageOn } from "react-icons/ci";
+import { IoCameraOutline } from "react-icons/io5";
+import { FaMicrophone } from "react-icons/fa6";
+import { MdOutlineEmojiEmotions } from "react-icons/md";
+import { IoMdSend } from "react-icons/io";
+import EmojiPicker from "emoji-picker-react";
 
 function Chat() {
+  const [toggleEmojie, setToggleEmojie] = useState(false);
+  const [text, setText] = useState("");
+  function handelEmojie(e) {
+    console.log(text);
+    setText(text + e.emoji);
+  }
   return (
     <div className="chatContainer">
-      chat
+      <div className="chatTop">
+        <div className="topUserInfo">
+          <div className="userInfoImg">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3twOSZJs7lNZa0V0sU3xOWz_zMUVn7k-bXMzYnfrJJw&s"
+              alt=""
+            />
+          </div>
+          <div className="userInfoText">
+            <span>Subhajit Ghosh</span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="topIcons">
+          <IoIosCall className="topReactIcons" />
+          <IoIosVideocam className="topReactIcons" />
+          <ImInfo className="topReactIcons" />
+        </div>
+      </div>
+      <div className="chatCenter"></div>
+      <div className="chatBottom">
+        <div className="leftIcons">
+          <CiImageOn className="topReactIcons" />
+          <IoCameraOutline className="topReactIcons" />
+          <FaMicrophone className="topReactIcons" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type your text here..."
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          value={text}
+        />
+        <div className="rightIcons">
+          <div className="emojieContainer">
+            <MdOutlineEmojiEmotions
+              className="topReactIcons emojieIcon"
+              onClick={() => {
+                setToggleEmojie(!toggleEmojie);
+              }}
+            />
+            <div className="reactEmojieContainer">
+              {toggleEmojie && (
+                <EmojiPicker
+                  className="emojieBox"
+                  onEmojiClick={(e) => handelEmojie(e)}
+                />
+              )}
+            </div>
+          </div>
+          <IoMdSend className="topReactIcons" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
--- src/Components/ChatListCard.jsx
+++ src/Components/ChatListCard.jsx
@@ -6,7 +6,7 @@ function ChatListCard() {
     <div className="ChatListCardContainer">
       <div className="itemImgContainer">
         <img
-          src="https://imgs.search.brave.com/7_EAqhx1XD9bh2yoP5E6bUMY_qGTy3qHa8QY9nzAt5c/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1QlptTXpZMlkw/WlRNdE5HVXdNUzAw/WldRekxUa3hZekl0/T0dSaVpqWmpOakJt/TVRsa1hrRXlYa0Zx/Y0dkZVFYVnlOelUx/TnpFM05UZ0AuanBn"
+          src="https://pics.craiyon.com/2023-09-18/61b305a522014300b6699216384f8b51.webp"
           alt=""
         />
       </div>
