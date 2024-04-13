3950950fed84958d634f559782be818d572298f7
Subhajitorrin <orrin2op@gmail.com>
2024-04-13 12:54:42 +0530

    update register circle

--- src/Pages/LoginRegister.jsx
+++ src/Pages/LoginRegister.jsx
@@ -125,7 +125,7 @@ function LoginRegister() {
               <img
                 src={
                   avatar.url ||
-                  "https://as1.ftcdn.net/v2/jpg/02/59/39/46/1000_F_259394679_GGA8JJAEkukYJL9XXFH2JoC3nMguBPNH.jpg"
+                  ""
                 }
                 ref={avatarRef}
               />
